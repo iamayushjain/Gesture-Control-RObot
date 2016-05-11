4747bb37c4f2cb413c03177b3e4f0a0ec076843f
iamayushjain <ayushjain2911@gmail.com>
2016-05-11 13:37:01 +0530

    first commit

---
+++ pro70.c
@@ -0,0 +1,223 @@
+/*****************************************************
+This program was produced by the
+CodeWizardAVR V2.05.2 Evaluation
+Automatic Program Generator
+© Copyright 1998-2011 Pavel Haiduc, HP InfoTech s.r.l.
+http://www.hpinfotech.com
+
+Project : 
+Version : 
+Date    : 7/4/2015
+Author  : Freeware, for evaluation and non-commercial use only
+Company : 
+Comments: 
+
+
+Chip type               : ATmega16
+Program type            : Application
+AVR Core Clock frequency: 8.000000 MHz
+Memory model            : Small
+External RAM size       : 0
+Data Stack size         : 256
+*****************************************************/
+
+#include <mega16.h>
+
+#include <delay.h>
+#include <stdlib.h>
+#include <delay.h>
+// Alphanumeric LCD functions
+#include <alcd.h>
+
+#define ADC_VREF_TYPE 0x00
+
+// Read the AD conversion result
+unsigned int read_adc(unsigned char adc_input)
+{
+ADMUX=adc_input | (ADC_VREF_TYPE & 0xff);
+// Delay needed for the stabilization of the ADC input voltage
+delay_us(10);
+// Start the AD conversion
+ADCSRA|=0x40;
+// Wait for the AD conversion to complete
+while ((ADCSRA & 0x10)==0);
+ADCSRA|=0x10;
+return ADCW;
+}
+
+// Declare your global variables here
+
+void main(void)
+{
+// Declare your local variables here
+        int x;
+        char y[10];
+        int a;
+        int b;
+        char z[10];
+        char u[10];
+// Input/Output Ports initialization
+// Port A initialization
+// Func7=In Func6=In Func5=In Func4=In Func3=In Func2=In Func1=In Func0=In 
+// State7=T State6=T State5=T State4=T State3=T State2=T State1=T State0=T 
+PORTA=0x00;
+DDRA=0x00;
+
+// Port B initialization
+// Func7=In Func6=In Func5=In Func4=In Func3=In Func2=In Func1=In Func0=In 
+// State7=T State6=T State5=T State4=T State3=T State2=T State1=T State0=T 
+PORTB=0x00;
+DDRB=0x00;
+
+// Port C initialization
+// Func7=In Func6=In Func5=In Func4=In Func3=In Func2=In Func1=In Func0=In 
+// State7=T State6=T State5=T State4=T State3=T State2=T State1=T State0=T 
+PORTC=0x00;
+DDRC=0x00;
+
+// Port D initialization
+// Func7=In Func6=In Func5=Out Func4=Out Func3=In Func2=In Func1=In Func0=In 
+// State7=T State6=T State5=0 State4=0 State3=T State2=T State1=T State0=T 
+PORTD=0x00;
+DDRD=0x30;
+
+// Timer/Counter 0 initialization
+// Clock source: System Clock
+// Clock value: Timer 0 Stopped
+// Mode: Normal top=0xFF
+// OC0 output: Disconnected
+TCCR0=0x00;
+TCNT0=0x00;
+OCR0=0x00;
+
+// Timer/Counter 1 initialization
+// Clock source: System Clock
+// Clock value: 125.000 kHz
+// Mode: Fast PWM top=0x00FF
+// OC1A output: Non-Inv.
+// OC1B output: Non-Inv.
+// Noise Canceler: Off
+// Input Capture on Falling Edge
+// Timer1 Overflow Interrupt: Off
+// Input Capture Interrupt: Off
+// Compare A Match Interrupt: Off
+// Compare B Match Interrupt: Off
+TCCR1A=0xA1;
+TCCR1B=0x0B;
+TCNT1H=0x00;
+TCNT1L=0x00;
+ICR1H=0x00;
+ICR1L=0x00;
+OCR1AH=0x00;
+OCR1AL=0x00;
+OCR1BH=0x00;
+OCR1BL=0x00;
+
+// Timer/Counter 2 initialization
+// Clock source: System Clock
+// Clock value: Timer2 Stopped
+// Mode: Normal top=0xFF
+// OC2 output: Disconnected
+ASSR=0x00;
+TCCR2=0x00;
+TCNT2=0x00;
+OCR2=0x00;
+
+// External Interrupt(s) initialization
+// INT0: Off
+// INT1: Off
+// INT2: Off
+MCUCR=0x00;
+MCUCSR=0x00;
+
+// Timer(s)/Counter(s) Interrupt(s) initialization
+TIMSK=0x00;
+
+// USART initialization
+// USART disabled
+UCSRB=0x00;
+
+// Analog Comparator initialization
+// Analog Comparator: Off
+// Analog Comparator Input Capture by Timer/Counter 1: Off
+ACSR=0x80;
+SFIOR=0x00;
+
+// ADC initialization
+// ADC Clock frequency: 1000.000 kHz
+// ADC Voltage Reference: AREF pin
+// ADC Auto Trigger Source: ADC Stopped
+ADMUX=ADC_VREF_TYPE & 0xff;
+ADCSRA=0x83;
+
+// SPI initialization
+// SPI disabled
+SPCR=0x00;
+
+// TWI initialization
+// TWI disabled
+TWCR=0x00;
+
+// Alphanumeric LCD initialization
+// Connections are specified in the
+// Project|Configure|C Compiler|Libraries|Alphanumeric LCD menu:
+// RS - PORTC Bit 0
+// RD - PORTC Bit 1
+// EN - PORTC Bit 2
+// D4 - PORTC Bit 4
+// D5 - PORTC Bit 5
+// D6 - PORTC Bit 6
+// D7 - PORTC Bit 7
+// Characters/line: 16
+lcd_init(16);
+                   DDRB=255;
+                   PORTB=5;  
+                   OCR1A=255;
+                   
+while (1)
+      {
+ x=read_adc(0);
+       a=read_adc(1);
+       b=read_adc(2);
+              // q=read_adc(1);
+              // a=x-q;
+               //w=(f1024;             
+               itoa(x,y);
+               itoa(a,z);
+               itoa(b,u);  
+                             lcd_putsf("x=  y=  z=  ");
+                              lcd_gotoxy(0,1);
+                            lcd_puts(y);
+                             lcd_gotoxy(5,1);
+                            lcd_puts(z);
+                             lcd_gotoxy(9,1);
+                            lcd_puts(u);
+                          
+                           
+                            if(x>=350)
+                            {
+                            PORTB=1;
+                            delay_ms(2000);
+                            }
+                           else if(x<=250)
+                            {
+                            PORTB=4;  
+                                   delay_ms(2000);
+                            }
+                                   
+                             if(a>=350)
+                            {
+                            PORTB=5;              
+                            delay_ms(2000);
+                            }      
+                            else if(a<=250)          
+                            {
+                            PORTB=10;      
+                            delay_ms(2000);  
+                            }   
+                            delay_ms(50);
+                              delay_ms(300);  
+                               lcd_clear();
+      }
+}                                                  
+
